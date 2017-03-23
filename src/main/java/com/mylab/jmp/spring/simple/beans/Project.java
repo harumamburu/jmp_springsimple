2b8f70ce8a6ea8d68be9e82c8c46217a9f830769
harumamburu <serbib92>
2017-03-24 00:17:13 +0300

    Add new bean class for projects

---
+++ src/main/java/com/mylab/jmp/spring/simple/beans/Project.java
@@ -0,0 +1,36 @@
+package com.mylab.jmp.spring.simple.beans;
+
+public class Project {
+
+    private String name;
+    private Developer lead;
+
+    public Project(String name, Developer lead) {
+        this.name = name;
+        this.lead = lead;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Developer getLead() {
+        return lead;
+    }
+
+    public void setLead(Developer lead) {
+        this.lead = lead;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "name='" + name + '\'' +
+                ", lead=" + lead +
+                '}';
+    }
+}
