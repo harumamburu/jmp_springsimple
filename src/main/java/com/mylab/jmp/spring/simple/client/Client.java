cac7c13b7461068c227fda030b14731a93e92d34
harumamburu <serbib92>
2017-03-24 00:25:10 +0300

    Refactor client code

--- src/main/java/com/mylab/jmp/spring/simple/client/Client.java
+++ src/main/java/com/mylab/jmp/spring/simple/client/Client.java
@@ -10,21 +10,18 @@ public class Client {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 
-        Developer developer = (Developer) getBean(context, "dev");
-        Developer javaDeveloper = (Developer) getBean(context, "javaDev");
-        Developer intern = (Developer) getBean(context, "intern");
+        getBean(context, "dev");
+        getBean(context, "javaDev");
 
+        Developer intern = (Developer) getBean(context, "intern");
         intern.setSkill("Kotlin");
         intern.setLevel(1);
         System.out.println(intern.toString());
 
-        Developer dotNetDeveloper = (Developer) getBean(context, "dotNetDev");
-
-        Developer hacker = (Developer) getBean(context, "hacker");
-
-        Project taxiProject = (Project) getBean(context, "taxiNuvo");
-
-        Project javaProject = (Project) getBean(context, "java10");
+        getBean(context, "dotNetDev");
+        getBean(context, "hacker");
+        getBean(context, "taxiNuvo");
+        getBean(context, "java10");
     }
 
     private static Object getBean(ApplicationContext context, String id) {
